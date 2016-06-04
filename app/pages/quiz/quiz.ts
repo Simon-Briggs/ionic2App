5f576d5174f5b0ec39a5da7e850ed8c99fd45660
Simon-Briggs <simonsatoshi@gmail.com>
2016-06-04 10:35:43 +0100

    Bind some data on the quiz page

--- app/pages/quiz/quiz.ts
+++ app/pages/quiz/quiz.ts
@@ -5,7 +5,11 @@ import {Page, NavController, NavParams} from 'ionic-angular';
     providers: []
 })
 export class QuizPage {
-    public readme = 'asdfasdfasdfasdsf';
+    public readme = 'Thie test will give you a rating on the likelihood of **. Complete the following:';
+    
+    public toppings;
+    public rating;
+
 
     constructor(private nav: NavController,
         private navParams: NavParams) {
